e6a739963e54f404fbd15e4c2f52237c713feed4
Omar Lopes <omararaujolopes1@gmail.com>
2020-12-09 15:37:44 -0300

    09/12/2020, 14:43

--- main.cpp
+++ main.cpp
@@ -4,16 +4,17 @@
 #include<map>
 #include<string>
 #include<stack>
+#include<sstream>
 using namespace std;
 typedef pair< pair<int, int>, pair<int, int> > iiii;//vertice, peso, chegada, saida
 typedef vector<pair< pair<int, int>, pair<int, int> > >T;
 typedef pair<int,int>ii;
 typedef vector<ii>vi;
 T G[1024];
-map<string, string>id_nome;
-map<string,string>nome_id;
-map<string, int>id_vertice;
-map<int, string>vertice_id;
+
+map<string, int>nome_vertice;
+vector<string>n_v;
+map<int, string>vertice_nome;
 vector<int>pai(1024, -1);
 string id, nome;
 vector<int>dist(1024, __INT_MAX__/2);
@@ -21,16 +22,17 @@ stack<int>pilha;
 
 int achaHora(string id, int p) // de chegada
 {
+
   for(auto it : G[p])
     {
-      if(vertice_id[it.first.first] == id) return it.second.first;
+      if(vertice_nome[it.first.first] == id){return it.second.first;}
     }
 }
 int achaHoraSaida(string id, int p)
 {
   for(auto it : G[p])
     {
-      if(vertice_id[it.first.first] == id) return it.second.second;
+      if(vertice_nome[it.first.first] == id){ return it.second.second;}
     }
 }
 void djistra(int s)
@@ -52,7 +54,7 @@ void djistra(int s)
           if(pai[u] != -1)
             {
               int p = pai[u];
-              int horaPaiChegou = achaHora(vertice_id[u], p);
+              int horaPaiChegou = achaHora(vertice_nome[u], p);
               if(saida - horaPaiChegou < 30) flag = false;
             }
           if(flag)
@@ -67,6 +69,7 @@ void djistra(int s)
         }
     }
 }
+
 void achaCaminho(int a, int b)
 {
   pilha.push(b);
@@ -78,7 +81,7 @@ void achaCaminho(int a, int b)
   return;
 
 }
-int converteHora(string hora)
+int converteHora(string hora) // recebe a hora em string e retorna o equivalente em minutos
 {
   int aux = 0;
   aux+=(int(hora[0] - 48))*10;
@@ -90,59 +93,84 @@ int converteHora(string hora)
 }
 int main()
 {
-  cout<<"DIGITE A IDENTIFICACAO E O NOME DE TODOS OS AEROPORTOS, E QUANDO TERMINAR DIGITE #\n";
-  cin>>id;
-  int v = 1;
-
-  while (id.compare("#")) {
-      cin>>nome;
-      id_nome[id] = nome;
-      nome_id[nome] = id;
-      vertice_id[v] = id;
-      id_vertice[id] = v;
-      v++;
-      cin>>id;
-    }
   string saida, chegada;
   string nomeSegundo;
-  cout<<"DIGITE AS OPCOES DE VIAGENS, E QUANDO ACABAR DIGITE O #\n";
-  cin>>id;
-  while (id.compare("#")) {
-      cin>>nome;
-      cin>>saida;
 
-      cin>>nomeSegundo>>chegada;
-      int saidaNum, chegadaNum;
-      saidaNum=converteHora(saida);
-      chegadaNum = converteHora(chegada);
-      int peso = chegadaNum - saidaNum;
-      iiii auxi = iiii(make_pair(id_vertice[nome_id[nomeSegundo]], peso), make_pair(chegadaNum, saidaNum));
-      G[id_vertice[id]].push_back(auxi);
-      cin>>id;
+  cout<<"DIGITE AS OPCOES DE VIAGENS, E QUANDO ACABAR DIGITE O #\n";
+  int cont = 1;
+  string str, word;
+  getline(cin, str);
+  while (str.compare("#")) {
+
+      stringstream ss(str);
+      ss >> id;
+      map<string, int>::iterator itt;
+      ss >> nome;
+      //ss >> saida;
+
+      while (ss>>saida) {
+          //verifica o primeiro
+          ss>>nomeSegundo;
+          itt = nome_vertice.find(nome);
+
+          if(itt == nome_vertice.end()) // nao tem ainda no grafo
+            {
+              nome_vertice[nome] = cont;
+              vertice_nome[cont] = nome;
+              cont++;
+            }
+
+         // verifica o segundo
+          itt = nome_vertice.find(nomeSegundo);
+          if(itt == nome_vertice.end()) // nao tem ainda no grafo
+            {
+              nome_vertice[nomeSegundo] = cont;
+              vertice_nome[cont] = nomeSegundo;
+              cont++;
+            }
+
+          ss>>chegada;
+          int saidaNum, chegadaNum;
+          saidaNum=converteHora(saida);
+          chegadaNum = converteHora(chegada);
+          int peso = chegadaNum - saidaNum;
+          iiii auxi = iiii(make_pair(nome_vertice[nomeSegundo], peso), make_pair(chegadaNum, saidaNum));
+          G[nome_vertice[nome]].push_back(auxi);
+          nome = nomeSegundo;
+
+        }
+      getline(cin, str);
     }
+
+    for(int i = 1; i<=cont; i++)
+      {
+        cout<<vertice_nome[i]<<": ";
+        for(auto it : G[i])
+          {
+            cout<<vertice_nome[it.first.first]<<" ";
+          }
+        cout<<endl;
+      }
 
     cout<<"DIGITE O NOME DO AEROPORTO DE ORIGEM\n";
     cin>>nome;
     string nome2;
-    djistra(id_vertice[nome_id[nome]]);
+    djistra(nome_vertice[nome]);
     cout<<"DIGITE O NOME DO AEROPORTO DE DESTINO\n";
     cin>>nome2;
-
-
-    cout<<"vc ira gastar, na melhor hipotese: "<<dist[(id_vertice[nome_id[nome2]])]/60<<":"<<dist[(id_vertice[nome_id[nome2]])]%60<<"hrs"<<endl;
 
     while (pilha.size()) {
         pilha.pop();
       }
-    achaCaminho(id_vertice[nome_id[nome]], id_vertice[nome_id[nome2]]);
+    achaCaminho(nome_vertice[nome], nome_vertice[nome2]);
 
 
     pilha.pop();
     while (!pilha.empty()) {
         int p = pai[pilha.top()];
-        cout<<vertice_id[p]<<" "<<id_nome[vertice_id[p]]<<" "<<achaHoraSaida(vertice_id[pilha.top()], p)/60<<":"<<achaHoraSaida(vertice_id[pilha.top()], p)%60<<" ";
+        cout<<vertice_nome[p]<<" "<<achaHoraSaida(vertice_nome[pilha.top()], p)/60<<":"<<achaHoraSaida(vertice_nome[pilha.top()], p)%60<<" ";
         cout<<" ";
-        cout<<vertice_id[pilha.top()]<<" "<<id_nome[vertice_id[pilha.top()]]<<" "<<achaHora(vertice_id[pilha.top()], p)/60<<":"<<achaHora(vertice_id[pilha.top()], p)%60;
+        cout<<vertice_nome[pilha.top()]<<" "<<achaHora(vertice_nome[pilha.top()], p)/60<<":"<<achaHora(vertice_nome[pilha.top()], p)%60;
         pilha.pop();
         cout<<endl;
       }
